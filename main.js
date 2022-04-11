7e964980a38a5cbf445d7c90e9479e95e529123c
Yaroslav Levchenko <yl30042004@gmail.com>
2022-04-11 17:29:53 +0300

    hw5

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function hw5(string, lengthString, symbol, start = false) {
+    let symbolString = "",
+        result = "";
+
+    for (let i = 0; i < lengthString; i++) {
+        symbolString += symbol;
+    }
+
+    if (start === true) {
+        result = symbolString + string;
+    } else {
+        result = string + symbolString;
+    }
+    return result;
+}
+
+let result = hw5("smth", 8, "0", true);
+console.log(result);
